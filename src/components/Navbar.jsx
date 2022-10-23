833cf65221d8db3be819eb0e3b4b1d2a7034cd44
kallyas <kallyasmedia@gmail.com>
2022-10-23 23:00:58 +0300

    ch: add layout components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header id="header-container" className="fullwidth">
+      <div id="header">
+        <div className="container">
+          <div className="left-side"></div>
+          <div className="right-side"></div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
