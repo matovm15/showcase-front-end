e2b4a19e585af5044a77bea8f30f427054ca7679
kallyas <kallyasmedia@gmail.com>
2022-11-03 15:48:03 +0300

    ch: add app test data

---
+++ src/data/dashNotifications.js
@@ -0,0 +1,44 @@
+export const DASH_NOTIFICATIONS = [
+  {
+    id: 1,
+    title: "Full Stack Software Engineer",
+    details: {
+      name: "Michael Shannah",
+      message: "applied for a job",
+    },
+    icon: "icon-material-outline-group",
+  },
+  {
+    id: 2,
+    title: "Full Stack Software Engineer",
+    details: {
+      name: "Michael Shannah",
+      message: "applied for a job",
+    },
+    icon: "icon-material-outline-group",
+  },
+  {
+    id: 3,
+    title: "Full Stack Software Engineer",
+    details: {
+      name: "Michael Shannah",
+      message: "applied for a job",
+    },
+    icon: "icon-material-outline-group",
+  },
+  {
+    id: 4,
+    title: "Full Stack Software Engineer",
+    details: {
+      name: "Michael Shannah",
+      message: "applied for a job",
+    },
+    icon: "icon-material-outline-group",
+  },
+];
+
+
+export const profileViews = {
+  months:['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+  views:new Array(12).fill(0).map((v,i)=>Math.floor(Math.random()*100))
+}
